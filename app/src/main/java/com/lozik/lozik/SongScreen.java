d62961c6beba66a8619d5a0ba1532e1138bf5715
Bing-Yuan Hsieh <hsiehby@gmail.com>
2018-11-27 15:02:40 -0600

    Second Commit

---
+++ app/src/main/java/com/lozik/lozik/SongScreen.java
@@ -0,0 +1,13 @@
+package com.lozik.lozik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_screen);
+    }
+}
